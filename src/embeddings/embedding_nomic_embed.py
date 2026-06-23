1e015fe0bb1be06da661c2c6d95b87b623f01806
Nguyen Tien Hung <nthung06082k5@gmail.com>
2026-06-23 09:56:35 +0700

    change env for ollama host

--- src/embeddings/embedding_nomic_embed.py
+++ src/embeddings/embedding_nomic_embed.py
@@ -1,7 +1,9 @@
 from langchain_ollama.embeddings import OllamaEmbeddings
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 def get_nomic_embed():
     """
     Returns an instance of OllamaEmbeddings for Nomic embeddings.
     """
-    return OllamaEmbeddings(model="nomic-embed-text")
+    return OllamaEmbeddings(model="nomic-embed-text", base_url=os.getenv("OLLAMA_HOST"))
